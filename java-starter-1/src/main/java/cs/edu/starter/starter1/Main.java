1b976cd8f7bb8bf1c84018435aed2128a3eed117
ranxero <cschl@LAPTOP-S32QMBMT>
2023-05-06 15:31:36 +0200

    basic classes

---
+++ java-starter-1/src/main/java/cs/edu/starter/starter1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cs.edu.starter.starter1;
+
+/**
+ * @author c. schluessel
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
