f5843df61d7f39bf81d9489f330aef7fa0b840d9
ranxero <cschl@LAPTOP-S32QMBMT>
2023-05-06 17:22:46 +0200

    log4j2 added

--- java-starter-1/src/main/java/cs/edu/starter/starter1/Main.java
+++ java-starter-1/src/main/java/cs/edu/starter/starter1/Main.java
@@ -3,18 +3,27 @@
  */
 package cs.edu.starter.starter1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author c. schluessel
  *
  */
 public class Main {
 
+	private static final Logger logger = LogManager.getLogger(Main.class);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hellow World\ninitializing Delegate ...");
-		System.out.println(new Delegate().converstBitsToDecimal(new int[] {0, 1, 0, 0, 1, 1, 0, 1}));
+		logger.info("Hellow World ... initializing Delegate ...");
+		logger.debug(new Delegate().converstBitsToDecimal(new int[] {0, 1, 0, 0, 1, 1, 0, 1}));
+		
+		logger.trace("trace logging");
+		logger.warn("warn logging");
+        logger.error("error logging");
+        logger.fatal("fatal logging");
 	}
 
 }
